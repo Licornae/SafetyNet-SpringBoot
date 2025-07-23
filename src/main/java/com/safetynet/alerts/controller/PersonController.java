0d9dcf8ccfddf973adef2cebe10c92b0448f1a90
Caroline <bergere.caroline@outlook.fr>
2025-07-23 11:25:03 +0200

    test : faire passer le premier test sur PersonController avec une réponse simulée

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,5 +1,22 @@
 package com.safetynet.alerts.controller;
 
+import com.safetynet.alerts.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
-    //TODO endpoints Person
+    @GetMapping("/person")
+    public Person getPerson(@RequestParam String firstName, @RequestParam String lastName) {
+        Person person = new Person();
+        person.setFirstName("John");
+        person.setLastName("Boyd");
+        person.setAddress("1509 Culver St");
+        person.setCity("Culver");
+        person.setZip("97451");
+        person.setPhone("841-874-6512");
+        person.setEmail("jaboyd@email.com");
+        return person;
+    }
 }
