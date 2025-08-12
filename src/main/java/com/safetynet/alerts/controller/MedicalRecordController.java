2c49d72fe496c76252665c5d04c39b80d4d32cb5
Caroline <bergere.caroline@outlook.fr>
2025-08-12 15:59:57 +0200

    feat : faire passer le test endpoint GET /medicalRecord, ajout de l'implémentation

--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -1,5 +1,24 @@
 package com.safetynet.alerts.controller;
 
+import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.service.MedicalRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MedicalRecordController {
-    //TODO endpoints MedicalRecord
+
+    private final MedicalRecordService medicalRecordService;
+
+    @Autowired
+    public MedicalRecordController(MedicalRecordService medicalRecordService){
+        this.medicalRecordService = medicalRecordService;
+    }
+
+    @GetMapping("/medicalRecord")
+    public MedicalRecord getMedicalRecord(@RequestParam String firstName, @RequestParam String lastName){
+        return medicalRecordService.getMedicalRecord(firstName,lastName);
+    }
 }
