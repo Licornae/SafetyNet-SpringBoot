818dcda2762f09dadb4b4f368ba8adc6173f1fea
Caroline <bergere.caroline@outlook.fr>
2025-09-17 18:01:55 +0200

    test : ajout des tests FireServiceImplTest GET/ infos et station des résidents d'une adresse

---
+++ src/test/java/com/safetynet/alerts/service/FireServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.safetynet.alerts.service;
+
+import com.safetynet.alerts.exception.AddressNotFoundException;
+import com.safetynet.alerts.repository.DataRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class FireServiceImplTest {
+
+    @Autowired
+    DataRepository dataRepository;
+
+    @Autowired
+    FireService fireService;
+
+    @BeforeEach
+    void reload() {
+        dataRepository.reloadData();
+    }
+
+    @Test
+    public void testGetResidentsInfosAtTheAddress_returnsResidentsInfosAndStation(){
+
+        FireDTO fireDTO = fireService.getResidentsByAddress("1509 Culver St");
+
+        assertNotNull(fireDTO);
+        assertEquals(3, fireDTO.getStation());
+        assertNotNull(fireDTO.getResidents());
+    }
+
+    @Test
+    public void testGetUnknownAddress_throws404() {
+        assertThrows(AddressNotFoundException.class,
+                () -> fireService.getResidentsByAddress("Unknown Address"));
+    }
+
+    @Test
+    public void testGetBlank_throws400() {
+        assertThrows(IllegalArgumentException.class,
+                () -> fireService.getResidentsByAddress(" "));
+    }
+}
